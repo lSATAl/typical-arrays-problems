9e2f36d27864f4cf4f49ce6a1a819aa334b8e0d5
lSATAl <70383465+lSATAl@users.noreply.github.com>
2020-10-03 20:02:57 +0300

    feat: add solution for task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    if(array.length === 0) {
+      return 0;
+    } else {
+  let min = array[0];
+  for(let i = 0; i < array.length; i++) {
+    if (min > array[i]) min = array[i];
+  }
+  return min
+}
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[]) {
+    if(array.length === 0) {
+        return 0;
+    }
+    let max = array[0];
+    for(let i = 0; i < array.length; i++) {
+      if (max < array[i]) max = array[i];
+    }
+    return max
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array=[]) {
+    if (array === undefined) {
+        return 0;
+      }
+    if(array.length === 0) {
+        return 0;
+    }
+let summ = 0;
+  for(let i = 0; i < array.length; i++) {
+    summ += array[i]
+  }
+  return summ / array.length
 }
